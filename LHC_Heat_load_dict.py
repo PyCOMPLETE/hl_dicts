f49360949c0350b259a338d22d0aa0d21ef647fc
LHC scrubbing <lhcscrub@lxplus0026.cern.ch>
2017-01-23 17:36:16 +0100

    moved some 025_ script

--- LHC_Heat_load_dict.py
+++ LHC_Heat_load_dict.py
@@ -3,8 +3,8 @@ import cPickle
 from dict_utils import *
 
 this_directory = os.path.dirname(os.path.abspath(__file__))
-dict_file_2016 = this_directory + '/large_heat_load_dict_2016.pkl'
-dict_file_2015 = this_directory + '/large_heat_load_dict_2015.pkl'
+dict_file_2016 = this_directory + '/large_heat_load_dict_2016_latest.pkl'
+dict_file_2015 = this_directory + '/large_heat_load_dict_2015_latest.pkl'
 
 with open(dict_file_2016, 'r') as f:
     main_dict_2016 = cPickle.load(f)
