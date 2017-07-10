ecfba3b210a8b1d501acce02d8c3f55d8bf3b806
giadarol <giovanni.iadarola@cern.ch>
2017-07-10 19:47:34 +0200

    Status

--- 016_plot_hl_from_dicionaries.py
+++ 016_plot_hl_from_dicionaries.py
@@ -26,6 +26,9 @@ parser.add_argument('--moment', help='Sample at %s' % sorted(list_moments))
 parser.add_argument('--custom_vars', help='Custom list of variables to plot', nargs='+', default=[])
 parser.add_argument('--colormap', help='chose between hsv and rainbow', default='hsv')
 parser.add_argument('--normtointensity', help='Normalize to beam intensity', action='store_true')
+parser.add_argument('--full-varname-in-legend', help='Do not shorten varnames.', action='store_true')
+parser.add_argument('--savein', help='Specify folder to save the output', default='cell_by_cell_plots')
+
 
 parser.add_argument('--min_nbun', help='Remove fills with less than given number of bunches per beam', type=int)
 
@@ -38,7 +41,7 @@ if len(args.custom_vars)>0:
     group_names.append('Custom')
     dict_hl_groups['Custom'] = args.custom_vars
 
-
+moment=args.moment
 
 
 
@@ -62,14 +65,14 @@ fontsz = 16
 
 
 pl.close('all')
-ms.mystyle_arial(fontsz=fontsz, dist_tick_lab=5)
+ms.mystyle_arial(fontsz=fontsz, dist_tick_lab=8)
 
 N_figures = len(group_names)
 figs = []
 figs_integ = []
 sp1 = None
 for ii, group_name in enumerate(group_names):
-    fig_h = pl.figure(ii, figsize=(8*1.6, 6*1.6))
+    fig_h = pl.figure(ii, figsize=(8*1.8, 6*1.4))
     figs.append(fig_h)
     fig_h.patch.set_facecolor('w')
         
@@ -86,7 +89,7 @@ for ii, group_name in enumerate(group_names):
     
     #plot n bunches
     spint.plot(x_axis, hldict[moment]['n_bunches']['b2'], '.r', markersize=markersize)
-    spint.plot(x_axis, hldict[moment]['n_bunches']['b2'], '.b', markersize=markersize) 
+    spint.plot(x_axis, hldict[moment]['n_bunches']['b1'], '.b', markersize=markersize) 
     
     
     varnames = dict_hl_groups[group_name]
@@ -94,6 +97,21 @@ for ii, group_name in enumerate(group_names):
     for i_var, var in enumerate(varnames):
         colorcurr = ms.colorprog(i_prog=i_var, Nplots=len(varnames), cm=args.colormap)
         
+        # prepare label
+        if args.full_varname_in_legend:
+            label = var
+        else:
+            label = ''
+            for st in var.split('.POSST')[0].split('_'):
+                if 'QRL' in st or 'QBS' in st or 'AVG' in st or 'ARC' in st:
+                    pass
+                else:
+                    label += st + ' '
+            label = label[:-1]
+        
+        
+        
+        # compute normalized heat load
         normhl = hldict[moment]['heat_load']['ldb_naming'][var]/(hldict[moment]['intensity']['b1']+\
                         hldict[moment]['intensity']['b2'])
         
@@ -103,19 +121,52 @@ for ii, group_name in enumerate(group_names):
             to_plot = hldict[moment]['heat_load']['ldb_naming'][var]
 
         # plot heatload
-        sphlcell.plot(x_axis, to_plot, '.', color = colorcurr, markersize=markersize)
+        sphlcell.plot(x_axis, to_plot, '.', color = colorcurr, markersize=markersize, label=label)
         
         #plot integrated
         integ_hl_this = np.cumsum(hldict['hl_integrated']['ldb_naming'][var])
-        spinteg.plot(integ_hl_this, normhl, '.', color=colorcurr, markersize=markersize)
+        spinteg.plot(integ_hl_this, normhl, '.', color=colorcurr, markersize=markersize, label=label)
         
 
+    spinteg.set_ylabel('Normalized heat load [W/p+]')
+    spinteg.set_xlabel('Normalized heat load [J]')
+
+    spint.set_ylabel('Number of bunches')
+    if args.normtointensity:
+        sphlcell.set_ylabel('Normalized heat load [W/p+]')
+    else:
+        sphlcell.set_ylabel('Heat load [W/p+]')
+    sphlcell.set_xlabel('Fill number')
+    sphlcell.legend(prop={'size':fontsz}, bbox_to_anchor=(1.05, 1.0),  loc='upper left')
     
 
+    for sp in [spint, sphlcell, spinteg]:
+        sp.grid('on')
+        sp.set_ylim(bottom=0)
 
-for sp in [spint, sphlcell, spinteg]:
-    sp.grid('on')
-    sp.set_ylim(bottom=0)
+    figinteg_h.subplots_adjust(bottom=.13)
+    fig_h.subplots_adjust(hspace=.32, right=.77)
+    
+    
+    for fig in [figinteg_h, fig_h]:
+        fig.suptitle(group_name)
+        
+
+def save_evol(infolder='./'):
+    if args.normtointensity:
+        strfile = 'normheatload'
+    else:
+        strfile = 'heatload'
+    for fig in figs:
+        fig.savefig(infolder+'/'+strfile+'_evol_'+fig._suptitle.get_text()+'.png',  dpi=200)
+
+
+def save_integ(infolder='./'):
+    strfile = 'scrubcurve_'
+
+    for fig in figs_integ:
+        fig.savefig(infolder+'/'+strfile+'_evol_'+fig._suptitle.get_text()+'.png',  dpi=200)
 
 
 pl.show()
+
